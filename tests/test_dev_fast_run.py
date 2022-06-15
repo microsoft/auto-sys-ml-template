be5ebdab0a66c0599b2b7ae74eff8f15336a6eaf
Ratnesh Madaan <ratneshmadaan@gmail.com>
2022-06-15 04:46:56 -0700

    add pytest to azure pipeline (#19)

--- tests/test_dev_fast_run.py
+++ tests/test_dev_fast_run.py
@@ -12,3 +12,17 @@ def test_fast_dev_run():
         "--trainer.fast_dev_run=true",
     ]
     run_command(command)
+
+
+# cpu only test for CI
+def test_fast_dev_run_cpu():
+    """Test running for 1 train, val and test batch."""
+    command = [
+        "src/train.py",
+        "-c",
+        "configs/train.yaml",
+        "--trainer.fast_dev_run=true",
+        "--trainer.accelerator=cpu",
+        "--trainer.sync_batchnorm=false",
+    ]
+    run_command(command)
